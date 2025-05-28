4ce519340ebefc91d14fd81bbf150fc7b35921a7
cerongithub <developerceron@gmail.com>
2025-05-28 10:41:28 +0800

    query done

--- jquery/create-new-data.php
+++ jquery/create-new-data.php
@@ -5,32 +5,41 @@
     $password = "password";    
 
     $connectionString = new mysqli($servername,$username,$password);    
-    
+        
+
     !empty($_POST["person-who-recommend"]);
     !empty($_POST["gcash"]);
     !empty($_POST["fullname"]);
     !empty($_POST["phone-number"]);
     !empty($_POST["location"]);
-    !empty($_POST["date"]);
-    !empty($_POST["time"]);
+    !empty($_POST["date0"]);
+    !empty($_POST["date1"]);
+    !empty($_POST["time0"]);
+    !empty($_POST["time1"]);
     !empty($_POST["sex"]);
     !empty($_POST["accepted-terms-and-conditions"]);
     !empty($_POST["package"]);
 
+    $package = ($_POST["package"]);
     $PWR = ($_POST["person-who-recommend"]);
     $gcash = ($_POST["gcash"]);
     $fn = ($_POST["fullname"]);
     $pn = ($_POST["phone-number"]);
     $loc = ($_POST["location"]);
-    $date = ($_POST["date"]);
-    $time = ($_POST["time"]);
-    $sex = ($_POST["sex"]);
-    $package = ($_POST["package"]);
+
+    $date0 = ($_POST["date0"]);
+    $date1 = ($_POST["date1"]);
+    
+    $sdate = date("Y-m-d", strtotime($date0));
+    $edate = date("Y-m-d", strtotime($date1));    
+
+    $stime = ($_POST["time0"]);
+    $etime = ($_POST["time1"]);
+    $sex = ($_POST["sex"]);    
     $terms_and_conditions = ($_POST["accepted-terms-and-conditions"]);
     $data_created = date("F j, Y, g:i a");
-
-
-    $query1 = "INSERT INTO resort.client VALUES (0,'$PWR', '$package','$fn', '$pn', '$date', '$time', '$loc', '$sex', '$terms_and_conditions', null, '$data_created')";
+    
+    $query1 = "INSERT INTO resort.client VALUES (0,'$PWR', '$package','$fn', '$pn', '$sdate', '$edate', '$stime', '$etime', '$loc', '$sex', '$terms_and_conditions', null, '$data_created')";
     if(!empty($gcash)){
         $query2 = "INSERT INTO resort.`person-who-recommend` VALUES(0,'$PWR', '$gcash', '$package', 800)";
         $recordData1 = mysqli_query($connectionString, $query2);
--- js/reservation.js
+++ js/reservation.js
@@ -5,4 +5,20 @@ $(document).ready(function(){
     $package_input_fld = document.getElementById("package");
     $package_input_fld.value = $get_package;
     $package_input_fld.readOnly = true;
+
+        var array = ["2025-05-25","2025-05-26","2025-05-27"]    
+    $('#date0').datepicker({
+        minDate: '0',
+        beforeShowDay: function(date){
+            var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
+            return [ array.indexOf(string) == -1 ]
+        }
+    });
+    $('#date1').datepicker({
+        minDate: '0',
+        beforeShowDay: function(date){
+            var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
+            return [ array.indexOf(string) == -1 ]
+        }
+    });
 });
